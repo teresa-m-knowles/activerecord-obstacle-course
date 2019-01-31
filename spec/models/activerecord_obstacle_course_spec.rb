49d312d67abcbaed076906280a68eb1c2d1c7190
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-31 11:24:33 -0700

    9 done

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -195,11 +195,12 @@ describe 'ActiveRecord Obstacle Course' do
     expected_result = [@order_3, @order_6, @order_15, @order_9, @order_12]
 
     # ----------------------- Using Ruby -------------------------
-    orders_of_user_3 = Order.all.select { |order| order.user_id == @user_3.id }
+    # orders_of_user_3 = Order.all.select { |order| order.user_id == @user_3.id }
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
+    orders_of_user_3 = Order.where(user: @user_3.id)
     # ------------------------------------------------------------
 
     # Expectation
