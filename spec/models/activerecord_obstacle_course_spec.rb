f99080dab46b0d0a9ad5b69f11c844117d5221fd
Teresa Knowles <teresa07moreno@gmail.com>
2019-01-31 11:50:27 -0700

    12 done

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -255,11 +255,13 @@ describe 'ActiveRecord Obstacle Course' do
     ]
 
     # ----------------------- Using Ruby -------------------------
-    items = Item.all.map { |item| item unless items_not_included.include?(item) }.compact
+    # items = Item.all.map { |item| item unless items_not_included.include?(item) }.compact
     # ------------------------------------------------------------
 
     # ------------------ Using ActiveRecord ----------------------
     # Solution goes here
+
+    items = Item.where.not(id: items_not_included)
     # ------------------------------------------------------------
 
     # Expectation
