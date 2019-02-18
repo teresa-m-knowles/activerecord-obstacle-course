8c7c7b4ea14f5fe0d0a3c0fe6d8235e0681864a0
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-17 22:03:57 -0700

    28 done

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -637,8 +637,8 @@ describe 'ActiveRecord Obstacle Course' do
     expect(custom_results[2].total_order_count).to eq(5)
   end
 
-  xit '28. returns a table of information for all users items' do
-    custom_results = [@user_2, @user_3, @user_1]
+  it '28. returns a table of information for all users items' do
+    # custom_results = [@user_2, @user_3, @user_1]
 
     # using a single ActiveRecord call, fetch a joined object that mimics the
     # following table of information:
@@ -649,7 +649,10 @@ describe 'ActiveRecord Obstacle Course' do
     # Ian        |         20
 
     # ------------------ ActiveRecord Solution ----------------------
-    # custom_results =
+    custom_results = User.joins(:items)
+                         .select('users.name, count(items.id) as total_item_count')
+                         .group(:id)
+                         .order('users.name desc')
     # ---------------------------------------------------------------
 
     expect(custom_results[0].name).to eq(@user_3.name)
