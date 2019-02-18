203c2e5f533fceb95980bcaf5303f20321c74d69
Teresa Knowles <teresa07moreno@gmail.com>
2019-02-17 22:33:18 -0700

    done

--- spec/models/activerecord_obstacle_course_spec.rb
+++ spec/models/activerecord_obstacle_course_spec.rb
@@ -732,7 +732,7 @@ describe 'ActiveRecord Obstacle Course' do
     Bullet.start_request
 
     # ------------------------------------------------------
-    orders = Order.all # Edit only this line
+    orders = Order.includes(:items)
     # ------------------------------------------------------
 
     # Do not edit below this line
